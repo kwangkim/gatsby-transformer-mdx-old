2e48bc8bc021192802563d2289b5a9e9fb239362
KWANG KIM <devkwang@gmail.com>
2018-07-13 00:22:10 -0400

    katex

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,14 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
     const mdxFiles = /\.mdx$/; //mdx only. for md and mdx use /\.mdx?$/;
     config.loader(`mdx`, {
         test: mdxFiles,
-        loaders: ['babel-loader?' + 'babelrc=false,' + 'presets[]=env,' + 'presets[]=react', '@mdx-js/loader']
+        loaders: [
+            'babel-loader?' + 'babelrc=false,' + 'presets[]=env,' + 'presets[]=react',
+            {
+                loader: '@mdx-js/loader',
+                options:{
+                    mdPlugins: ['gatsby-remark-katex']
+                }
+            },
+        ]
     });
 };
